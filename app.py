257e53ef7a83d984bce248f8cce2def00a965a10
Pablo Ramon <ramonparaiba@gmail.com>
2024-05-21 13:20:32 -0300

    Arquivo base para Pull Request

---
+++ app.py
@@ -0,0 +1,7 @@
+Frutas = ['Java', 'Limão', 'Mamão', 'Banana', 'Morango', 'Cajú']
+
+
+#Altere o projeto da seguinte forma 
+#1 Imprima o tamanho da lista
+#2 Imprima cada item da lista seguido da quantidade de caracteres
+#3 Remova o último item da lista
